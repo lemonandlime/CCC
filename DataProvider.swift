88a659efe8fee438df10c712a618996f8a93f3d2
Karl Söderberg <karl.soderberg@gmail.com>
2015-11-04 00:24:08 +0100

    Added image downloading and caching with Alarmofire

--- DataProvider.swift
+++ DataProvider.swift
@@ -9,19 +9,24 @@
 import UIKit
 import SwiftyJSON
 import Alamofire
+import AlamofireImage
 
 
 
 private let _sharedInstance = DataProvider()
 private let baseUrl = "http://46.101.78.53:3000/"
 private let cache = NSCache()
+private let imageDownloader = ImageDownloader(
+    configuration: ImageDownloader.defaultURLSessionConfiguration(),
+    downloadPrioritization: .FIFO,
+    maximumActiveDownloads: 4,
+    imageCache: AutoPurgingImageCache()
+)
 
 private let kMainDataKey = "mainData"
 
 
 class DataProvider: NSObject {
-    
-    
     
     class var sharedInstance : DataProvider {
         return _sharedInstance
@@ -52,6 +57,34 @@ class DataProvider: NSObject {
                     onCompletion(Result.Success(episodes))
                 }
         }
+    }
+    
+    func getImageForEpisode(episode: Episode, size: ImageSize, onCompletion: Result<UIImage, NSError> -> Void) {
+        guard let imageUrl = episode.images?.thumb else {
+            let error = NSError(
+                domain: "CCCErrorDomain",
+                code: -10,
+                userInfo: [NSLocalizedDescriptionKey: "Missing url"])
+            onCompletion(Result.Failure(error))
+            return
+        }
+        
+        let URLRequest = NSURLRequest(URL: NSURL(string: imageUrl)!)
+        
+        imageDownloader.downloadImage(URLRequest: URLRequest) { (response) -> Void in
+            switch response.result {
+            case .Failure(let error):
+                onCompletion(Result.Failure(error))
+                
+            case .Success(let image):
+                onCompletion(Result.Success(image))
+            }
+        }
+    }
+    
+    enum ImageSize {
+        case Thumbnail
+        case Regular
     }
 }
 
