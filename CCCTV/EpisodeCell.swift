dabcbf3fdafe6c945de18eec9b70ae66b93bf96c
Karl Söderberg <karl.soderberg@screeninteraction.com>
2016-03-25 22:51:35 +0100

    Moved implementation of a lot of code closer to the real components that present the code.

--- CCCTV/EpisodeCell.swift
+++ CCCTV/EpisodeCell.swift
@@ -13,4 +13,47 @@ class EpisodeCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var guestsLabel: UILabel!
+    var onSelectedEpisode: ((Episode)->Void)!
+    
+    override var selected: Bool {
+        didSet {
+            if let episode = episode where selected != oldValue && selected{
+                onSelectedEpisode(episode)
+            }
+        }
+    }
+    
+    var episode: Episode? {
+        didSet {
+            if let episode = episode {
+                titleLabel.text = episode.title
+                guestsLabel.text = episode.guestsString;
+                
+                DataProvider.sharedInstance.getImageForEpisode(episode, size: .Thumbnail) { (result) -> Void in
+                    switch result {
+                    case .Failure(let error):
+                        print(error)
+                        
+                    case .Success(let image):
+                        self.imageView?.image = image
+                    }
+                }
+            }
+        }
+    }
+    
+    override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
+        coordinator.addCoordinatedAnimations({ () -> Void in
+            switch self.focused {
+            case true:
+                self.titleLabel.alpha = 1.0
+                self.guestsLabel.transform = CGAffineTransformMakeTranslation(0, 30)
+            case false:
+                self.titleLabel.alpha = 0.0
+                self.guestsLabel.transform = CGAffineTransformIdentity
+            }
+            
+            },
+            completion: nil)
+    }
 }
--- CCCTests/SeasonCell.swift
+++ CCCTests/SeasonCell.swift
@@ -16,12 +16,6 @@ class SeasonTableViewCell: UITableViewCell, UICollectionViewDataSource {
     var section: Int!
     var onSelectedEpisode: ((Episode)->Void)!
     
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-    
     // DataSource
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -38,59 +32,8 @@ class SeasonTableViewCell: UITableViewCell, UICollectionViewDataSource {
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! EpisodeCell
-        let episode = viewModel.dataForIndexPath(NSIndexPath(forRow: indexPath.item, inSection: section))
-        cell.titleLabel.text = episode.title;
-        cell.guestsLabel.text = episode.guestsString;
-        
-        provider.getImageForEpisode(episode, size: .Thumbnail) { (result) -> Void in
-            switch result {
-            case .Failure(let error):
-                print(error)
-                
-            case .Success(let image):
-                cell.imageView?.image = image
-                cell.setNeedsLayout()
-            }
-        }
-        
+        cell.episode = viewModel.dataForIndexPath(NSIndexPath(forRow: indexPath.item, inSection: section))
+        cell.onSelectedEpisode = onSelectedEpisode
         return cell
     }
-    
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        onSelectedEpisode(viewModel.dataForIndexPath(NSIndexPath(forItem: indexPath.item, inSection: section)))
-    }
-    
-    func collectionView(collectionView: UICollectionView, didUpdateFocusInContext context: UICollectionViewFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
-        super.didUpdateFocusInContext(context, withAnimationCoordinator: coordinator)
-        
-        if let indexPath = context.previouslyFocusedIndexPath {
-            let cell = collectionView.cellForItemAtIndexPath(indexPath) as! EpisodeCell
-            coordinator.addCoordinatedAnimations({ () -> Void in
-                self.focusCell(cell, focus: false)
-                }, completion: nil)
-            
-        }
-        
-        context.previouslyFocusedIndexPath
-        
-        if let indexPath = context.nextFocusedIndexPath {
-            let cell = collectionView.cellForItemAtIndexPath(indexPath) as! EpisodeCell
-            coordinator.addCoordinatedAnimations({ () -> Void in
-                self.focusCell(cell, focus: true)
-                }, completion: nil)
-        }
-    }
-    
-    private func focusCell(cell: EpisodeCell, focus: Bool) {
-        switch focus {
-        case true:
-            cell.titleLabel.alpha = 1.0
-            cell.guestsLabel.transform = CGAffineTransformMakeTranslation(0, 30)
-        case false:
-            cell.titleLabel.alpha = 0.0
-            cell.guestsLabel.transform = CGAffineTransformIdentity
-        }
-    }
-
-
 }
